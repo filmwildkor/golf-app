cfc0e1ed9d1590cd8e0953de0b2a67254382ac8f
filmwild <filmwildkor@gmail.com>
2026-03-16 14:17:24 +0900

    1분단위 드럼롤 + 스냅 + 캐시 갱신

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'golf-app-v1';
+const CACHE_NAME = 'golf-app-v3';
 const ASSETS = [
   '/golf-app/',
   '/golf-app/index.html',
@@ -10,6 +10,7 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
 // 활성화: 오래된 캐시 삭제
@@ -19,6 +20,7 @@ self.addEventListener('activate', e => {
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
+  self.clients.claim();
 });
 
 // 요청 가로채기: 인터넷 없으면 저장된 파일로 응답
